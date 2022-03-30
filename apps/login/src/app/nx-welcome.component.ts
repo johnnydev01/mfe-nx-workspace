fea05a76472b1a6aa0f4f46f08f352cc0c7d3846
Johnny Silva <johnnysilva.dev@gmail.com>
2022-03-30 10:04:48 -0300

    configuracao inicial micro front-end

---
+++ apps/login/src/app/nx-welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'ng-mfe-nx-welcome',
+  template: `
+      `,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
